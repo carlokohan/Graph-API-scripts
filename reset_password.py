e1e5ba36ec03aa604fbecdd01386885b38c8d95e
Carlo Husmillo <carlohusmillo@gmail.com>
2023-06-19 10:47:43 +0800

    Create reset_password.py
    reset the password of a user using graph api

---
+++ reset_password.py
@@ -0,0 +1,56 @@
+import requests
+import datetime
+import logging
+import time
+import json
+import traceback
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+#########
+
+login_url = 'https://login.microsoftonline.com/'
+resource = 'https://graph.microsoft.com/'
+
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+if __name__ == "__main__":
+
+    try:
+        client_id = 'xxx'
+        client_secret = 'xxx'
+        tenant_domain = 'xxx'
+
+        bodyvals = {'client_id': client_id,
+                    'client_secret': client_secret,
+                    'grant_type': 'client_credentials',
+                    'resource': resource}
+
+        request_url = login_url + tenant_domain + '/oauth2/token'
+        token_response = requests.post(request_url, data=bodyvals)
+
+        access_token = token_response.json().get('access_token')
+        token_type = token_response.json().get('token_type')
+
+        final_token = token_type + ' ' + access_token
+        header_params = {'Authorization': final_token, 'Content-Type': 'application/json'}
+        print(header_params)
+# https://learn.microsoft.com/en-us/graph/api/user-revokesigninsessions?view=graph-rest-1.0&tabs=http
+
+        # get object id
+        object_id = 'emailC@your_domain.onmicrosoft.com'
+        request_url = resource + 'v1.0/users/' + object_id + '/authentication/methods/xxxx/resetPassword'
+
+        response = requests.post(request_url, headers=header_params)
+        print(response.json())
+
+        if response.status_code == 200:
+            print("Done password reset")
+        else:
+            print("Failed password reset")
+
+    except Exception:
+        print("Error occured.")
+        print(traceback.format_exc())
